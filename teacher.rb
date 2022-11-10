ba4d816ca70438be3a32ded1ac358f57aeca477f
Elfin Git <musongoletracy@gmail.com>
2022-11-10 15:55:14 +0200

    create UI

--- teacher.rb
+++ teacher.rb
@@ -3,9 +3,11 @@ require_relative 'person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
-    super(name, age, parent_permission)
+  def initialize(age, specialization, name = 'Unknown')
+    super(name, age)
     @specialization = specialization
+    @name = name
+    @age = age
   end
 
   def can_use_services?
